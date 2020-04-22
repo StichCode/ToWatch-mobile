db2586591b086d8658b7765741fe888a28de851c
Aleksandr <rabbit_1399@icloud.com>
2020-04-22 17:13:36 +0300

    change to flatlist, and new button to add films

--- screens/MoviesScreen.js
+++ screens/MoviesScreen.js
@@ -1,132 +1,142 @@
-import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
-import { Image, Picker, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import rook from '../assets/images/rook.png';
+import { Image, FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+
+const DATA = [
+  {
+    id:         '1',
+    title:      'Ладья',
+    imagePath:  require('../assets/images/rook.png'),
+    tag:        'Сериал',
+  },
+  {
+    id:         '2',
+    title:      'Побег из Шоушенка',
+    imagePath:  require('../assets/images/run.png'),
+    tag:        'Фильм',
+  },
+  {
+    id:         '3',
+    title:      'Зеленая миля',
+    imagePath:  require('../assets/images/mile.png'),
+    tag:        'Фильм',
+  },
+  {
+    id:         '4',
+    title:      'Форрест Гамп',
+    imagePath:  require('../assets/images/gamp.png'),
+    tag:        'Фильм',
+  },
+  {
+    id:         '5',
+    title:      '1+1',
+    imagePath:  require('../assets/images/oneplus.png'),
+    tag:        'Фильм',
+  },
+  
+];
+
+const colorTag = {
+  'Фильм': 'yellow',
+  'Сериал': 'cyan',
+  'Мультфильм': 'green'
+};
+
+
+
+function Record({ id, title, im, tag}) {
+  return (
+    <TouchableOpacity style={styles.cartFilm}>
+      <View style={{flex: 1, flexDirection: 'column'}}>
+          <Image source={im} style={styles.picture}/>
+          <View style={styles.movieBblock}>
+            <Text style={styles.titleMovie}>{title}</Text>
+            <View style={{backgroundColor: colorTag[tag], marginLeft: 30, marginTop: 5, width: 80, borderRadius: 10}}>
+              <Text style={{margin: '1%', marginLeft: 10}}>
+                {tag}
+              </Text>
+            </View>
+          </View>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
 
 
 export default function MoviesScreen() {
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-      <View style={{flexDirection:"row", padding: 10, marginLeft: '7%'}}>
-        <TouchableOpacity style={styles.cartFilm}>
-              <View style={styles.movieBlock}>
-                  <Image source={rook} style={{width: 105, height: 145, marginLeft: 20, marginTop: '5%', borderRadius: 10}}/>
-                  <View style={styles.movieBblock}>
-                    <Text style={{marginTop: '5%', fontSize: 15, marginLeft: 10,}}>Ладья 2019</Text>
-                    <View style={{marginLeft: 10, marginTop: 5, backgroundColor: 'cyan', width: 80, borderRadius: 10}}>
-                      <Text style={{margin: '1%', marginLeft: 10 }}>
-                        Сериал
-                      </Text>
-                    </View>
-                  </View>
-              </View>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.cartFilm} >
-              <View style={styles.movieBlock}>
-                  <Image source={rook} style={{width: 105, height: 145, marginLeft: 20, marginTop: '5%', borderRadius: 10}}/>
-                  <View style={styles.movieBblock}>
-                    <Text style={{marginTop: '5%', fontSize: 15, marginLeft: 10,}}>Ладья 2019</Text>
-                    <View style={{marginLeft: 10, marginTop: 5, backgroundColor: 'cyan', width: 80, borderRadius: 10}}>
-                      <Text style={{margin: '1%', marginLeft: 10 }}>
-                        Сериал
-                      </Text>
-                    </View>
-                  </View>
-              </View>
-          </TouchableOpacity>
-        </View>
-        <View style={{flexDirection:"row", padding: 10, marginLeft: '7%'}}>
-        <TouchableOpacity style={styles.cartFilm}>
-              <View style={styles.movieBlock}>
-                  <Image source={rook} style={{width: 105, height: 145, marginLeft: 20, marginTop: '5%', borderRadius: 10}}/>
-                  <View style={styles.movieBblock}>
-                    <Text style={{marginTop: '5%', fontSize: 15, marginLeft: 10,}}>Ладья 2019</Text>
-                    <View style={{marginLeft: 10, marginTop: 5, backgroundColor: 'cyan', width: 80, borderRadius: 10}}>
-                      <Text style={{margin: '1%', marginLeft: 10 }}>
-                        Сериал
-                      </Text>
-                    </View>
-                  </View>
-              </View>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.cartFilm}>
-              <View style={styles.movieBlock}>
-                  <Image source={rook} style={{width: 105, height: 145, marginLeft: 20, marginTop: '5%', borderRadius: 10}}/>
-                  <View style={styles.movieBblock}>
-                    <Text style={{marginTop: '5%', fontSize: 15, marginLeft: 10,}}>Ладья 2019</Text>
-                    <View style={{marginLeft: 10, marginTop: 5, backgroundColor: 'cyan', width: 80, borderRadius: 10}}>
-                      <Text style={{margin: '1%', marginLeft: 10 }}>
-                        Сериал
-                      </Text>
-                    </View>
-                  </View>
-              </View>
-          </TouchableOpacity>
-        </View>
-        <View style={{flexDirection:"row", padding: 10, marginLeft: '7%'}}>
-        <TouchableOpacity style={styles.cartFilm}>
-              <View style={styles.movieBlock}>
-                  <Image source={rook} style={{width: 105, height: 145, marginLeft: 20, marginTop: '5%', borderRadius: 10}}/>
-                  <View style={styles.movieBblock}>
-                    <Text style={{marginTop: '5%', fontSize: 15, marginLeft: 10,}}>Ладья 2019</Text>
-                    <View style={{marginLeft: 10, marginTop: 5, backgroundColor: 'cyan', width: 80, borderRadius: 10}}>
-                      <Text style={{margin: '1%', marginLeft: 10 }}>
-                        Сериал
-                      </Text>
-                    </View>
-                  </View>
-              </View>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.cartFilm}>
-              <View style={styles.movieBlock}>
-                  <Image source={rook} style={{width: 105, height: 145, marginLeft: 20, marginTop: '5%', borderRadius: 10}}/>
-                  <View style={styles.movieBblock}>
-                    <Text style={{marginTop: '5%', fontSize: 15, marginLeft: 10,}}>Ладья 2019</Text>
-                    <View style={{marginLeft: 10, marginTop: 5, backgroundColor: 'cyan', width: 80, borderRadius: 10}}>
-                      <Text style={{margin: '1%', marginLeft: 10 }}>
-                        Сериал
-                      </Text>
-                    </View>
-                  </View>
-              </View>
-          </TouchableOpacity>
-        </View>
-          
-        
-        
-      </ScrollView>
-
+      <FlatList 
+      data={DATA}
+      renderItem={({item}) => 
+        <Record
+          id ={item.id}
+          title={item.title}
+          im={item.imagePath}
+          tag={item.tag}
+        />
+      }
+      keyExtractor={item => item.id}
+      numColumns={'2'}
+      />
+      <TouchableOpacity style={styles.fab}>
+        <Text style={styles.fabIcon}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 
+
 const styles = StyleSheet.create({
+  cartFilm: {
+    borderWidth: 0,
+    borderRadius: 50,
+    borderColor: (0,255,255),
+    margin: 5,
+    marginLeft: 15
+  },
+
   movieBlock: {
     flex: 1,
     flexDirection: 'column',
   },
-  picture: {
-    width: 105,
-    height: 105,
-    borderRadius: 200,
-    marginLeft: 20,
-    marginTop: '5%',
+
+  titleMovie: {
+    marginTop: 5, 
+    fontSize: 15, 
+    marginLeft: 30,
   },
+
+  picture: {
+    width: 135, 
+    height: 175, 
+    marginLeft: 10, 
+    marginTop: 10, 
+    borderRadius: 10
+  },
+
+  fab: {
+    position: 'absolute',
+    width: 56,
+    height: 56,
+    alignItems: 'center',
+    justifyContent: 'center',
+    right: 20,
+    bottom: 20,
+    backgroundColor: '#03A9F4',
+    borderRadius: 30,
+    elevation: 8
+  },
+  fabIcon: {
+    fontSize: 40,
+    color: 'white'
+  },
+
   movieBblock: {
     flex: 1,
     flexDirection: 'column',
-  },
-
-  cartFilm: {
-      borderWidth: 0,
-      borderRadius: 50,
-      borderColor: (255,255,255),
-      margin: 10,
-      height: 200,
-      width: 150,
   },
 
   cartText: {
@@ -142,7 +152,7 @@ const styles = StyleSheet.create({
   },
 
   contentContainer: {
-    paddingTop: 30,
+    paddingTop: 20,
   },
-  
+
 });
